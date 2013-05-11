35c2f8c1a7e4d4e9b0ef2cf7098e347a18d11ec1
Adam Achterhoff <achterhoff_83@hotmail.com>
2013-05-10 23:01:11 -0500

    Third Commit
    -Added Main Menu image. Notify me if you want it changed
    -Implemented a form of modality with the frames
    -Cleaned up code for RandomMaze and MazeTester classes
    -Added comments to RandomMaze and MazeTester classes (If you want to
    proof read the comments, that would be awesome)

--- src/maze/RandomMaze.java
+++ src/maze/RandomMaze.java
@@ -4,12 +4,11 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class RandomMaze extends JFrame {
-
+	//Declare instance variables
 	private int rowN = MainMenu.getBoardSize();
 	private int colN = MainMenu.getBoardSize();
 	private Cell[][] board = new Cell[rowN][colN];
@@ -22,9 +21,9 @@ public class RandomMaze extends JFrame {
 	private JLabel jlblStatus = new JLabel("Started..");
 	private boolean pressed = false;				// added 5/8 JB
 	private boolean pathFound = false;
-
-
+	private static MainMenu mainFrame;
 	public RandomMaze() {
+		//Setup the GUI
 		setTitle("Random Maze");
 		setSize(500,500);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -32,19 +31,15 @@ public class RandomMaze extends JFrame {
 		setVisible(true);
 		jpBoard = new JPanel();
 		jpBoard.setLayout(new GridLayout(rowN, colN));
-
 		jpButton = new JPanel();
 		jpButton.setLayout(new FlowLayout());
 		jpButton.add(jbtFindPath);
 		jpButton.add(jbtClearPath);
 		jpButton.add(jbtNewMaze);			// added 5/8 JB
 		jpButton.add(jbtClose);				// added 5/8 JB
-
-
 		board = createRandomMaze(testBoard);
 		for (int row = 0; row < board.length; row++) {
 			for (int col = 0; col < board[row].length; col++) {
-				//				board[row][col] = new Cell();
 				jpBoard.add(board[row][col]);
 			}
 		}
@@ -61,7 +56,6 @@ public class RandomMaze extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				if(pathFound){
 					clearPath();
-					//resetPath();
 					findPath();
 				}else{
 					findPath();
@@ -86,7 +80,7 @@ public class RandomMaze extends JFrame {
 						jpBoard.add(board[row][col]);
 					}
 				}
-				revalidate();
+				validate();
 				clearPath();
 				setMarkers();
 			}
@@ -95,28 +89,19 @@ public class RandomMaze extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				closeWindow();
-
 			}
 		});
 	}
-
-
-
-
-
 	// Main method
 	public static void main(String[] args) {
 		// Create a frame
 		RandomMaze frame = new RandomMaze();
 	}
-
-
-
-
-
-
-
-	// recursive method to find a path from start to finish
+	//Method used to set the parent frame (the MainMenu frame)
+	public void setParent(MainMenu parentFrame){
+		mainFrame = parentFrame;
+	}
+	//Method determines if path exists or not
 	public void findPath() {
 		if (findPath(0, 0)) {
 			jlblStatus.setText("path found");
@@ -126,129 +111,90 @@ public class RandomMaze extends JFrame {
 			jlblStatus.setText("No path exists");
 		}
 	}
-
+	//Method used to recursively find a traversable path
 	public boolean findPath(int row, int col) {
 		board[row][col].visit();
-
+		//Base case
+		//If the path is at the end, return true
 		if ((col == colN-1) && (row == rowN-1)) {
 			board[row][col].selectCell();
-			//repaint();				//added 5/8 AA
 			return true;
 		}
-		if ((row < rowN-1) && !board[row + 1][col].marked() &&
-				!board[row + 1][col].blocked() && !board[row + 1][col].visited()) {
+		//If the path can move down one cell, do so
+		if ((row < rowN-1) && !board[row + 1][col].marked() &&!board[row + 1][col].blocked() && !board[row + 1][col].visited()) {
 			block(row, col);
-
 			if (findPath(row + 1, col)) {
 				board[row][col].selectCell();
-				//repaint();				//added 5/8 AA
 				return true;
 			}
-
 			unblock(row, col);
 		}
-		if ((col < colN-1) && !board[row][col + 1].marked() &&
-				!board[row][col + 1].blocked() && !board[row][col + 1].visited()) {
+		//If the path can move to the right one cell, do so
+		if ((col < colN-1) && !board[row][col + 1].marked() && !board[row][col + 1].blocked() && !board[row][col + 1].visited()) {
 			block(row,col);
 			if (findPath(row, col + 1)) {
 				board[row][col].selectCell();
-				//repaint();				//added 5/8 AA
 				return true;
 			}
-
 			unblock(row,col);
 		}
-		if ((row > 0) && !board[row - 1][col].marked() &&
-				!board[row - 1][col].blocked() && !board[row - 1][col].visited()) {
+		//If the path can move up one cell, do so
+		if ((row > 0) && !board[row - 1][col].marked() && !board[row - 1][col].blocked() && !board[row - 1][col].visited()) {
 			block(row, col);
-
 			if (findPath(row - 1, col)) {
 				board[row][col].selectCell();
-				//repaint();				//added 5/8 AA
 				return true;
 			}
-
 			unblock(row, col);
 		}
-		if ((col > 0) && !board[row][col - 1].marked() &&
-				!board[row][col - 1].blocked() && !board[row][col - 1].visited()) {
+		//If the path can move to the left one cell, do so.
+		if ((col > 0) && !board[row][col - 1].marked() && !board[row][col - 1].blocked() && !board[row][col - 1].visited()) {
 			block(row,col);
 			if (findPath(row, col - 1)) {
 				board[row][col].selectCell();
-				//repaint();				//added 5/8 AA
 				return true;
 			}
-
 			unblock(row,col);
 		}
 		return false;
-
 	}
-
-
-
-	// Temporary block the neighbor to prevent neighboring path
+	//Block adjacent cell to prevent neighboring path
 	public void block(int row, int col) {
-		if (row > 0) {
+		if (row > 0)
 			board[row - 1][col].block();
-		}
-
-		if (row < rowN-1) {
+		if (row < rowN-1)
 			board[row + 1][col].block();
-		}
-
-		if (col > 0) {
+		if (col > 0) 
 			board[row][col - 1].block();
-		}
-
-		if (col < colN-1) {
+		if (col < colN-1)
 			board[row][col + 1].block();
-		}
 	}
-
-
-
-	// Remove the temporary block
+	//Remove the block if within range of the array
 	public void unblock(int row, int col) {
-		if (row > 0) {
+		if (row > 0)
 			board[row - 1][col].unblock();
-		}
-
-		if (row < rowN-1) {
+		if (row < rowN-1)
 			board[row + 1][col].unblock();
-		}
-
-		if (col > 0) {
+		if (col > 0)
 			board[row][col - 1].unblock();
-		}
-
-		if (col < colN-1) {
+		if (col < colN-1)
 			board[row][col + 1].unblock();
-		}
 	}
-
-
-	// method for closing the window
+	//Method for closing the window
 	public void closeWindow(){
 		WindowEvent winClosingEvent = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
 		Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(winClosingEvent);
+		mainFrame.setEnabled(true);		//Added 5/10 AA
 	}
-
-
-
-	// method to only clear the path
+	//Method to only clear the path
 	public void clearPath() {
 		for (int row = 0; row < board.length; row++) {
 			for (int col = 0; col < board[row].length; col++) {
 				board[row][col].deselectCell();
 			}
 		}
-		//repaint();	//added 5/8 AA
 	}
-
-
-
-	// clearAll method to clear the entire maze board
+	//Method to clear the board
 	public void clearAll() {								// added 5/8 JB
 		for (int row = 0; row < board.length; row++) {
 			for (int col = 0; col < board[row].length; col++) {
@@ -256,34 +202,22 @@ public class RandomMaze extends JFrame {
 			}
 		}
 	}
-
-
-
-	// getter method to determine if the mouse is pressed
+	//Getter method to determine if the mouse is pressed
 	public boolean isPressed(){			// added 5/8 JB
 		return pressed;
 	}
-
-	// setter method to set the mouse pressed variable
+	//Setter method to set the mouse pressed variable
 	public void setPressed(boolean e){		// added 5/8 JB
 		pressed = e;
 	}
-
-
-
-
 	// method to add start and finish graphics to the board
 	public void setMarkers(){
 		board[0][0].setStart();
 		board[rowN-1][colN-1].setFinish();
 	}
-
-
-
-	// method to reset the path
+	//Method to reset the path
 	protected void resetPath() {
 		pathFound = false;
-		//		repaint();
 		for (int row = 0; row < board.length; row++) {
 			for (int col = 0; col < board[row].length; col++) {
 				board[row][col].unblock();
@@ -291,14 +225,18 @@ public class RandomMaze extends JFrame {
 				unblock(row, col);
 			}
 		}
-		revalidate();
+		validate();
 		repaint();
 	}
+	//Method used to create a random maze in the passed Cell array
+	//and return a populate Cell array
 	public Cell[][] createRandomMaze(Cell[][] nBoard){
+		//Populate array
 		for (int row = 0; row < nBoard.length; row++) {
 			for (int col = 0; col < nBoard[row].length; col++) {
 				nBoard[row][col] = new Cell();
 				if((row > 0 && col > 0)){
+					//Create a random instance to set the cell as marked
 					int randSeed = (int)(Math.random() * 100 + 1);
 					if(randSeed > 50)
 						nBoard[row][col].setMarked();
@@ -306,6 +244,8 @@ public class RandomMaze extends JFrame {
 			}
 		}
 		MazeTester newMaze = new MazeTester(nBoard, rowN, colN );
+		//If the new maze is not traversable, then call the method again
+		//This way the method will eventually create a traversable maze
 		if(!newMaze.isTraversable())
 			createRandomMaze(nBoard);
 		return nBoard;
@@ -318,25 +258,20 @@ public class RandomMaze extends JFrame {
 			System.out.println();
 		}
 	}
-
-
-
-
-
-
-
-	// Inner class
+	//Inner Cell class
 	class Cell extends JPanel implements MouseListener {
-		//		private BufferedImage image;
+		//Declare instance variables
 		private boolean marked = false;
 		private boolean visited = false;
 		private boolean blocked = false;
 		private boolean start = false;
 		private boolean finish = false;
+		//Constructor
 		public Cell() {
 			setBackground(Color.DARK_GRAY);
 			addMouseListener(this);
 		}
+		//Accessor methods
 		public void setStart(){
 			start = true;
 		}
@@ -374,10 +309,12 @@ public class RandomMaze extends JFrame {
 		public void unblock() {
 			blocked = false;
 		}
+		//Method used to indicate the cell is selected as a path cell
 		public void selectCell() {
 			setBackground(Color.red);
 			repaint();
 		}
+		//Method used to return the cell to an unselected state
 		public void deselectCell() {
 			if(visited){
 				setBackground(Color.DARK_GRAY);
@@ -386,7 +323,7 @@ public class RandomMaze extends JFrame {
 			blocked = false;
 			repaint();
 		}
-
+		//Method used to reset the cell completely
 		public void resetCell(){				// added 5/8 JB
 			setBackground(Color.DARK_GRAY);
 			marked = false;
@@ -395,7 +332,8 @@ public class RandomMaze extends JFrame {
 			repaint();
 
 		}
-
+		//Method used to draw the cells corresponding graphics for either the 
+		//start, finish, or marked cells
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
@@ -415,23 +353,28 @@ public class RandomMaze extends JFrame {
 					Image scaledImage = image.getScaledInstance(getSize().width, getSize().height, Image.SCALE_DEFAULT);
 					g.drawImage(scaledImage, 0 ,0,null);
 				}
-
 			}catch(IOException ex){};
 		}
-		public void mouseClicked(MouseEvent e) {
-		}
+		//Unused implemented method
+		public void mouseClicked(MouseEvent e) {}
+		//Implemented method used to drag and set the marked value to true
+		//as long as it is not a start or finish cell when the cell is entered
+		//by the mouse
 		public void mouseEntered(MouseEvent e) {
 			if(isPressed() && !isStart() && !isFinish()){
-				marked = !marked;		//added 5/8 AA
+				marked = true;
 				repaint();
 			}
-
 		}
-		public void mouseExited(MouseEvent e) {
-		}
+		//Unused implemented method
+		public void mouseExited(MouseEvent e) {}
+		//Implemented method used to change its pressed status to false
+		//when the mouse is released
 		public void mouseReleased(MouseEvent e) {
 			setPressed(false);
 		}
+		//Implemented method used to set the cell as either marked or unmarked
+		//as long as it is not a start or finish cell when clicked upon
 		public void mousePressed(MouseEvent e) {
 			if(!isStart() && !isFinish()){
 				setPressed(true);
